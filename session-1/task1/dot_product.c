03e00e9bd5248f160dfba94b7169b16448559f01
sallide <gvzh0213@leeds.ac.uk>
2026-02-16 12:43:25 +0000

    ccc

--- session-1/task1/dot_product.c
+++ session-1/task1/dot_product.c
@@ -8,12 +8,22 @@
  int main( void ) {
     float a[]={ 1.0,1.0,1.0,1.0,1.0 };
     float b[]={ 2.0,3.0,4.0,5.0,6.0 };
-    float d;
+    float d= 0.0;
 
     /*
     Code to compute the dot product
     Store your answer in d
     print your final answer
     */
+
+  { 
+   for (int i = 0; i < 5; i++)
+        d += a[i]*b[i];
+   }
+   
+   printf("%f\n",d);
+
+
+
     return 0;
  }
--- session-1/task1/factorial.c
+++ session-1/task1/factorial.c
@@ -7,10 +7,30 @@
 
  int main( void ) {
     int f[20];
+    int n;
+    int d;
 
     /*
     Code to compute the factorial of each array index
     Print your final answer
+
+     for (int i = 0; i < 20; i++) {
+        printf("%2d    | %d\n", i, f[i]);
+    }
+
+
     */
+
+          { 
+   for (int i = 1; i < 20; i++)
+        d *= i; 
+   }
+
+
+   printf("%2d", d);
+
+    
+
+
     return 0;
  }
