210473db5a0683ead6b32786860e1855a672bcd9
sallide <gvzh0213@leeds.ac.uk>
2026-02-22 23:55:44 +0000

    almost done

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -9,11 +9,42 @@ Student ID:
 
 int main( int argc, char **argv ) {
 
-    // define appropriate data to hold your answer
+    // define appropriate data to hold your answer 
+    
+    //char buffer[100];
+
+
+    if (argc != 2) {
+    printf("Usage: ./makeString buffer\n");
+    return 1;
+}
+
+
+    char *input = argv[1];
+    
+
 
     // process the command-line data using appropriate string functions
 
+    size_t len = strlen(input);
+
+    char *buffer = malloc(len + 1);
+
+    if (buffer == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    strcpy(buffer, input);
+
+    for (size_t i = 0; i < len; i++) {
+        if (buffer[i] == ' ') {
+            buffer[i] = '-';
+        }
+    }
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
+
+    free(buffer);
     return 0;
 }
