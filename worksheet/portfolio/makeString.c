f1d81ad9a2391fb6e3c14ee56f5f9e9e470d40f7
sallide <gvzh0213@leeds.ac.uk>
2026-02-23 21:59:06 +0000

    sfe

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -12,20 +12,25 @@ int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer 
     
-    //char buffer[100];
 
 
-    if (argc != 2) {
-    printf("Usage: ./makeString buffer\n");
+    if (argc < 2) {
+    printf("./makeString buffer\n");
     return 1;
 }
+    size_t len = 0;
 
+    for (int i = 1; i < argc; i++) {
+        len += strlen(argv[i]);
+    }
+
+    len += (argc - 2);
 
     char *input = argv[1];
     
     // process the command-line data using appropriate string functions
 
-    size_t len = strlen(input);
+    //size_t len = strlen(input);
 
     char *buffer = malloc(len + 1);
 
@@ -34,16 +39,20 @@ int main( int argc, char **argv ) {
         return 1;
     }
 
-    strcpy(buffer, input);
+    buffer[0] = '\0';
 
-    for (size_t i = 0; i < len; i++) {
-        if (buffer[i] == ' ') {
-            buffer[i] = '-';
+    for (int i = 1; i < argc; i++) {
+        strcat(buffer, argv[i]);
+
+        if (i < argc - 1) {
+            strcat(buffer, "-");
         }
     }
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+
+    printf("%s\n", buffer); // use only this single print statement in your submitted code
 
 
     free(buffer);
     return 0;
 }
+
